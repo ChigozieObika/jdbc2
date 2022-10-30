e703d58ffd0f5f4728fbbaeb7a6c84455072b86b
Chigozie <obikacd@yahoo.co.uk>
2022-10-29 20:58:36 -0600

    implement autocloseable try

--- src/main/java/com/bptn/course/jdbc/JdbcExample4.java
+++ src/main/java/com/bptn/course/jdbc/JdbcExample4.java
@@ -23,55 +23,42 @@ public class JdbcExample4 {
 		String sql1 = "INSERT INTO students (\"studentId\", \"courseId\", \"studentName\"," +
 					 "\"studentEmail\", \"studentPhone\") VALUES (?, ?, ?, ?, ?)"; // insert statement for students table
 		
-		Connection conn = null;
-		try { // used a try-catch to handle a checked exception for creating a connection object
-			conn = DriverManager.getConnection(conStr, username, password); 
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		
-		try ( PreparedStatement stmt = conn.prepareStatement(sql); // Autocloseable for PreparedStatement objects
-				PreparedStatement stmt1 = conn.prepareStatement(sql1);
-			  ){
-			
+//		Connection conn = null;
+		try (Connection conn = DriverManager.getConnection(conStr, username, password);){ // used a try-catch to handle a checked exception for creating a connection object
+			 
 			conn.setAutoCommit(false); // set auto-commit to false
 			
-			stmt.setString(1, "Java");
-			
-			stmt1.setInt(1, 4);
-			stmt1.setInt(2, 5);
-			stmt1.setString(3, "Chigozie");
-			stmt1.setString(4, "Chigozies@bptn.com");
-			stmt1.setInt(5, 2546029);
-			
-			int num = stmt.executeUpdate();
-			int num1 = stmt1.executeUpdate();
-			
-			conn.commit();
-			
-			System.out.println("Number of rows inserted into courses table: " + num);
-			
-			System.out.println("Number of rows inserted into students table: " + num1);
-			
-			System.out.println("Query Executed Successfully!!");
-		} catch (SQLException e) { // catch of autocloseable try-catch
-			try { // try-catch for roll back
+			try ( PreparedStatement stmt = conn.prepareStatement(sql); // Autocloseable for PreparedStatement objects
+					PreparedStatement stmt1 = conn.prepareStatement(sql1);
+				  ){
+				
+				stmt.setString(1, "Java");
+				
+				stmt1.setInt(1, 6);
+				stmt1.setInt(2, 3);
+				stmt1.setString(3, "Chigozie");
+				stmt1.setString(4, "Chigozies@bptn.com");
+				stmt1.setInt(5, 2546029);
+				
+				int num = stmt.executeUpdate();
+				int num1 = stmt1.executeUpdate();
+				
+				conn.commit();
+				
+				System.out.println("Number of rows inserted into courses table: " + num);
+				
+				System.out.println("Number of rows inserted into students table: " + num1);
+				
+				System.out.println("Query Executed Successfully!!");
+			} catch (SQLException e) { // catch of autocloseable try-catch
+				System.out.println("Error while running query");
 				conn.rollback();
-			} catch (SQLException e1) {
-				System.out.println("Error while rolling back");
-				e1.printStackTrace();
-			}
-			System.out.println("Error while running query");
-			e.printStackTrace();
-		} 
-		finally { // close connection to database
-			if (conn != null) {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			}
+				System.out.println("Transaction rolled back");
+				System.out.println("Exception:" + e.getMessage());
+			} 
+			
+		} catch (SQLException e1) {
+			System.out.println("Exception:" + e1.getMessage());
 		}
 	}
 }
